ad5f25e92f40e4bfce30b1f4f6b7ab92f8a2bd4b
Robert Philipp <rob.philipp@gmail.com>
2022-03-20 08:21:18 -0400

    added return types to the sql-ast visitor

--- select/SqlToAstVisitor.ts
+++ select/SqlToAstVisitor.ts
@@ -7,6 +7,96 @@ const parserInstance = new SelectParser()
 // the base visitor class
 const BaseSqlVisitor = parserInstance.getBaseCstVisitorConstructor<SelectParser, any>()
 
+export class SqlToAstVisitor extends BaseSqlVisitor {
+    constructor() {
+        super()
+        this.validateVisitor()
+    }
+
+    selectStatement(ctx: SelectStatementContext): Partial<SelectSqlAst> {
+        // this.visit is used to visit non-terminal nodes (i.e. they have children) and
+        // will invoke the correct visit method for each child CST node
+        const select = this.visit(ctx.selectClause)
+
+        // this.visit can work on either a CstNode or an Array<CstNodes>.
+        const from = this.visit(ctx.fromClause)
+
+        // this.visit will ignore empty arrays when the clause is optional
+        const where = this.visit(ctx.whereClause)
+
+        return {
+            type: "SELECT_SMT",
+            selectClause: select,
+            fromClause: from,
+            whereClause: where
+        }
+    }
+
+    selectClause(ctx: SelectClauseContext): SelectClauseType {
+        const columns = ctx.Identifier.map((token: IToken) => token.image)
+
+        return {
+            type: "SELECT_CLAUSE",
+            columns
+        }
+    }
+
+    fromClause(ctx: FromClauseContext): FromClauseType {
+        const tableName = ctx.Identifier[0].image
+
+        return {
+            type: "FROM_CLAUSE",
+            table: tableName
+        }
+    }
+
+    whereClause(ctx: WhereClauseContext): WhereClauseType {
+        const condition = this.visit(ctx.expression)
+        return {
+            type: "WHERE_CLAUSE",
+            condition
+        }
+    }
+
+    expression(ctx: ExpressionContext): ExpressionType {
+        const lhs = this.visit(ctx.lhs[0])
+        const operator = this.visit(ctx.relationalOperator)
+        const rhs = this.visit(ctx.rhs[0])
+        return {
+            type: "EXPRESSION",
+            lhs, operator, rhs
+        }
+    }
+
+    atomicExpression(ctx: AtomicExpressionContext): string {
+        if (ctx.Integer) {
+            return ctx.Integer[0].image
+        }
+        return ctx.Identifier[0].image
+    }
+
+    relationalOperator(ctx: RelationalOperatorContext): string {
+        if (ctx.GreaterThan) {
+            return ctx.GreaterThan[0].image
+        }
+        return ctx.LessThan[0].image
+    }
+}
+
+const toAstVisitorInstance = new SqlToAstVisitor()
+
+export function toAst(input: string) {
+    const {parserInstance, cst} = parse(input)
+    // automatically creates the CST when parsing
+    // const cst = parseResult.selectStatement()
+
+    const ast = toAstVisitorInstance.visit(cst)
+    return ast
+}
+
+/*
+ | SUPPORTING TYPES
+ */
 type SelectStatementContext = CstChildrenDictionary & {
     selectClause: Array<CstNode>
     fromClause: Array<CstNode>
@@ -43,90 +133,34 @@ type RelationalOperatorContext = CstChildrenDictionary & {
     LessThan: Array<IToken>
 }
 
-
-export class SqlToAstVisitor extends BaseSqlVisitor {
-    constructor() {
-        super()
-        this.validateVisitor()
-    }
-
-    selectStatement(ctx: SelectStatementContext) {
-        // this.visit is used to visit non-terminal nodes (i.e. they have children) and
-        // will invoke the correct visit method for each child CST node
-        const select = this.visit(ctx.selectClause)
-
-        // this.visit can work on either a CstNode or an Array<CstNodes>.
-        const from = this.visit(ctx.fromClause)
-
-        // this.visit will ignore empty arrays when the clause is optional
-        const where = this.visit(ctx.whereClause)
-
-        return {
-            type: "SELECT_SMT",
-            selectClause: select,
-            fromClause: from,
-            whereClause: where
-        }
-    }
-
-    selectClause(ctx: SelectClauseContext) {
-        const columns = ctx.Identifier.map((token: IToken) => token.image)
-
-        return {
-            type: "SELECT_CLAUSE",
-            columns
-        }
-    }
-
-    fromClause(ctx: FromClauseContext) {
-        const tableName = ctx.Identifier[0].image
-
-        return {
-            type: "FROM_CLAUSE",
-            table: tableName
-        }
-    }
-
-    whereClause(ctx: WhereClauseContext) {
-        const condition = this.visit(ctx.expression)
-        return {
-            type: "WHERE_CLAUSE",
-            condition
-        }
-    }
-
-    expression(ctx: ExpressionContext) {
-        const lhs = this.visit(ctx.lhs[0])
-        const operator = this.visit(ctx.relationalOperator)
-        const rhs = this.visit(ctx.rhs[0])
-        return {
-            type: "EXPRESSION",
-            lhs, operator, rhs
-        }
-    }
-
-    atomicExpression(ctx: AtomicExpressionContext) {
-        if (ctx.Integer) {
-            return ctx.Integer[0].image
-        }
-        return ctx.Identifier[0].image
-    }
-
-    relationalOperator(ctx: RelationalOperatorContext) {
-        if (ctx.GreaterThan) {
-            return ctx.GreaterThan[0].image
-        }
-        return ctx.LessThan[0].image
-    }
+/*
+ | RETURN TYPES
+ */
+type SelectClauseType = {
+    type: string
+    columns: Array<string>
 }
 
-const toAstVisitorInstance = new SqlToAstVisitor()
+type FromClauseType = {
+    type: string,
+    table: string
+}
 
-export function toAst(input: string) {
-    const {parserInstance, cst} = parse(input)
-    // automatically creates the CST when parsing
-    // const cst = parseResult.selectStatement()
+type ExpressionType = {
+    type: string
+    lhs: string
+    operator: string
+    rhs: string
+}
 
-    const ast = toAstVisitorInstance.visit(cst)
-    return ast
+type WhereClauseType = {
+    type: string,
+    condition: ExpressionType
+}
+
+type SelectSqlAst = {
+    type: string
+    selectClause: SelectClauseType
+    fromClause: FromClauseType
+    whereClause: WhereClauseType
 }
